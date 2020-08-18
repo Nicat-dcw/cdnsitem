ec20adbf78b71cfc86ed183469f5e450fbf0fa64
Nicat.dcw <59221034+Nicat-dcw@users.noreply.github.com>
2020-08-18 23:20:52 +0400

    Create api.js

---
+++ dosya/api.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express ();
+
+app.get("/api/ncsifre", (req,res) => {
+
+var keys = ["NCassadashjzggasgj"]
+var keys2 = ["NCDJKKONHJKKALLLLD"]
+let result = keys
+res.json(keys);
+});
